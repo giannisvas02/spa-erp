5945e26fcb974da4713d9b5a19ed7aa5e53139a1
Ioannis Vasileiou <i.vasileiou@bpc.gr>
2025-08-19 12:42:09 +0300

    add vs support and successfully connect native app with webapp

--- src/SpaERP.NativeApp/MainPage.xaml.cs
+++ src/SpaERP.NativeApp/MainPage.xaml.cs
@@ -1,12 +1,18 @@
-﻿namespace SpaERP.NativeApp;
+﻿
+using System.Net.Http.Json;
+using SpaERP.Models;
+
+namespace SpaERP.NativeApp;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	HttpClient Client;
 
-	public MainPage()
+	public MainPage(HttpClient client)
 	{
 		InitializeComponent();
+		Client = client; // Use the injected HttpClient instance
 	}
 
 	private void OnCounterClicked(object? sender, EventArgs e)
@@ -19,5 +25,17 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private async void OnGetUsersClicked(object? sender, EventArgs e)
+	{
+
+		var users = await Client.GetFromJsonAsync<List<User>>("api/users");
+		if (users == null || !users.Any())
+		{
+			await DisplayAlert("Error", "No users found.", "OK");
+			return;
+		}
+		await DisplayAlert("Users", string.Join(", ", users.Select(u => $"{u.FirstName} {u.LastName}")), "OK");
 	}
 }
--- src/SpaERP.NativeApp/MauiProgram.cs
+++ src/SpaERP.NativeApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
 using Microsoft.Maui.Animations;
 
 namespace SpaERP.NativeApp;
@@ -15,6 +16,27 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		string serverUrl = "http://localhost:5000/";
+		Console.WriteLine(AppContext.BaseDirectory);
+		var configPath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+		if (!File.Exists(configPath))
+		{
+			configPath = Path.Combine(AppContext.BaseDirectory, "appsettings.user.json");
+		}
+
+		if (File.Exists(configPath))
+		{
+			var json = File.ReadAllText(configPath);
+			var config = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+			if (config != null && config.ContainsKey("ServerUrl"))
+				serverUrl = config["ServerUrl"];
+		}
+
+		builder.Services.AddSingleton(new HttpClient
+		{
+			BaseAddress = new Uri(serverUrl) // Adjust the base address as needed
+		});
 
 #if DEBUG
 		builder.Logging.AddDebug();
